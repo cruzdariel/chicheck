49d8232d2354124856d03a014ff4b90811a3f88d
Dariel Cruz Rodriguez <33551491+cruzdariel@users.noreply.github.com>
2025-03-05 12:01:05 -0600

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,3 +1,13 @@
 # EXCLUDE THIS FILE FROM THE GIT-IGNORE
 # IMPORT THIS FILE USING `from secrets import *`
 # DO NOT SHARE THIS FILE AT ALL! THESE KEYS ARE LIKE YOUR PASSWORDS!
+
+# Okta configuration
+OKTA_DOMAIN = ""
+OKTA_CLIENT_ID = ""
+OKTA_CLIENT_SECRET = ""
+
+# Discord configuration
+DISCORD_BOT_TOKEN = ""
+GUILD_ID = 0  # Replace with your Discord server (guild) ID (as an integer)
+ROLE_ID = 0   # Replace with the role ID to assign (as an integer)
